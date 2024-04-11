940cd2b9b99fda559c12a4e5cdb8dd70cc1876ff
alexhadzhiev35 <alexhadzhiev35@gmail.com>
2024-04-11 03:09:56 +0300

    Add newsletter component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, NewsletterComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, NewsletterComponent],
 })
 export class CoreModule {}
